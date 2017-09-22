5754000409d09fc7133a0879727b0a8c871c70f3
peisuke <peisuke.com@gmail.com>
2017-09-22 06:35:39 +0000

    without predict

--- caffe/imagenet/classify.py
+++ caffe/imagenet/classify.py
@@ -14,21 +14,30 @@ PRETRAINED = './bvlc_googlenet.caffemodel'
 
 caffe.set_mode_gpu()
 
-net = caffe.Classifier(MODEL_FILE, PRETRAINED,
-               mean=np.load('./ilsvrc_2012_mean.npy').mean(1).mean(1),
-               channel_swap=(2,1,0),
-               raw_scale=255,
-               image_dims=(224, 224))
+mean = np.load('./ilsvrc_2012_mean.npy').mean(1).mean(1)
+channel_swap = (2,1,0)
+raw_scale = 255
+image_dims=(224, 224)
 
-def caffe_predict(path):
-    input_image = caffe.io.load_image(path)
-    prediction = net.predict([input_image])
-    proba = prediction[0][prediction[0].argmax()]
-    ind = prediction[0].argsort()[-5:][::-1] # top-5 predictions
-    return prediction[0].argmax(), proba, ind
+cnn = caffe.Net(MODEL_FILE, PRETRAINED, caffe.TEST)
+
+in_ = cnn.inputs[0]
+transformer = caffe.io.Transformer({in_: cnn.blobs[in_].data.shape})
+transformer.set_transpose(in_, (2, 0, 1))
+transformer.set_raw_scale(in_, raw_scale)
+transformer.set_mean(in_, mean)
+transformer.set_channel_swap(in_, channel_swap)
+
+input_image = caffe.io.load_image('cat.jpg')
+input_image = caffe.io.resize_image(input_image, image_dims)
+input_image = transformer.preprocess(cnn.inputs[0], input_image)
+input_image = input_image[np.newaxis, :, :, :]
+
+cnn.blobs['data'].data[...] = input_image 
+output = cnn.forward()
+pred = output[cnn.outputs[0]][0]
 
 # load ImageNet labels
 labels = np.loadtxt('synset_words.txt', str, delimiter='\t')
 
-p, prob, _ = caffe_predict('./cat.jpg')
-print(labels[p], prob)
+print(labels[pred.argmax()], pred.max())
