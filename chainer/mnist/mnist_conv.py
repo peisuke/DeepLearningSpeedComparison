9defce509bb73b11cf2ea8faec368528e0e071a2
peisuke <peisuke.com@gmail.com>
2017-09-26 15:17:44 +0000

    fix chainer

--- chainer/mnist/mnist_conv.py
+++ chainer/mnist/mnist_conv.py
@@ -16,7 +16,7 @@ import tqdm
 # Network definition
 class MLP(chainer.Chain):
 
-    def __init__(self, n_out):
+    def __init__(self):
         super(MLP, self).__init__()
         with self.init_scope():
             # the size of the inputs to each layer will be inferred
@@ -24,7 +24,7 @@ class MLP(chainer.Chain):
             self.c2 = L.Convolution2D(None, 128, 3, 1, 1)  # n_units -> n_units
             self.c3 = L.Convolution2D(None, 256, 3, 1, 1)  # n_units -> n_out
             self.l4 = L.Linear(None, 512)
-            self.l5 = L.Linear(None, n_out)
+            self.l5 = L.Linear(None, 10)
 
     def __call__(self, x):
         h1 = F.relu(self.c1(x))
@@ -33,53 +33,11 @@ class MLP(chainer.Chain):
         h4 = F.relu(self.l4(h3))
         return self.l5(h4)
 
+# Load the MNIST dataset
+_, test = chainer.datasets.get_mnist(ndim=3)
 
-def main():
-    parser = argparse.ArgumentParser(description='Chainer example: MNIST')
-    parser.add_argument('--batchsize', '-b', type=int, default=100,
-                        help='Number of images in each mini-batch')
-    parser.add_argument('--epoch', '-e', type=int, default=20,
-                        help='Number of sweeps over the dataset to train')
-    parser.add_argument('--frequency', '-f', type=int, default=-1,
-                        help='Frequency of taking a snapshot')
-    parser.add_argument('--gpu', '-g', type=int, default=-1,
-                        help='GPU ID (negative value indicates CPU)')
-    parser.add_argument('--out', '-o', default='result',
-                        help='Directory to output the result')
-    parser.add_argument('--resume', '-r', default='',
-                        help='Resume the training from snapshot')
-    parser.add_argument('--unit', '-u', type=int, default=1000,
-                        help='Number of units')
-    args = parser.parse_args()
+model = MLP()
 
-    print('GPU: {}'.format(args.gpu))
-    print('# unit: {}'.format(args.unit))
-    print('# Minibatch-size: {}'.format(args.batchsize))
-    print('# epoch: {}'.format(args.epoch))
-    print('')
-
-    # Set up a neural network to train
-    # Classifier reports softmax cross entropy loss and accuracy at every
-    # iteration, which will be used by the PrintReport extension below.
-    model = MLP(10)
-    if args.gpu >= 0:
-        # Make a specified GPU current
-        chainer.cuda.get_device_from_id(args.gpu).use()
-        model.to_gpu()  # Copy the model to the GPU
-
-    # Setup an optimizer
-    optimizer = chainer.optimizers.Adam()
-    optimizer.setup(model)
-
-    # Load the MNIST dataset
-    train, test = chainer.datasets.get_mnist(ndim=3)
-
-    test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
-                                                 repeat=False, shuffle=False)
-
-    for img, label in tqdm.tqdm(test):
-        x_data = chainer.Variable(img[np.newaxis,:,:,:])
-        ret = F.softmax(model(x_data)).data
-
-if __name__ == '__main__':
-    main()
+for img, label in tqdm.tqdm(test):
+    x_data = chainer.Variable(img[np.newaxis,:,:,:])
+    ret = F.softmax(model(x_data)).data
--- chainer/mnist/mnist_mlp.py
+++ chainer/mnist/mnist_mlp.py
@@ -2,8 +2,6 @@
 
 from __future__ import print_function
 
-import argparse
-
 import chainer
 import chainer.functions as F
 import chainer.links as L
@@ -16,66 +14,23 @@ import tqdm
 # Network definition
 class MLP(chainer.Chain):
 
-    def __init__(self, n_units, n_out):
+    def __init__(self):
         super(MLP, self).__init__()
         with self.init_scope():
             # the size of the inputs to each layer will be inferred
-            self.l1 = L.Linear(None, n_units)  # n_in -> n_units
-            self.l2 = L.Linear(None, n_units)  # n_units -> n_units
-            self.l3 = L.Linear(None, n_out)  # n_units -> n_out
+            self.l1 = L.Linear(784, 1000)  # n_in -> n_units
+            self.l2 = L.Linear(1000, 1000)  # n_units -> n_units
+            self.l3 = L.Linear(1000, 10)  # n_units -> n_out
 
     def __call__(self, x):
         h1 = F.relu(self.l1(x))
         h2 = F.relu(self.l2(h1))
         return self.l3(h2)
 
+model = MLP()
 
-def main():
-    parser = argparse.ArgumentParser(description='Chainer example: MNIST')
-    parser.add_argument('--batchsize', '-b', type=int, default=100,
-                        help='Number of images in each mini-batch')
-    parser.add_argument('--epoch', '-e', type=int, default=20,
-                        help='Number of sweeps over the dataset to train')
-    parser.add_argument('--frequency', '-f', type=int, default=-1,
-                        help='Frequency of taking a snapshot')
-    parser.add_argument('--gpu', '-g', type=int, default=-1,
-                        help='GPU ID (negative value indicates CPU)')
-    parser.add_argument('--out', '-o', default='result',
-                        help='Directory to output the result')
-    parser.add_argument('--resume', '-r', default='',
-                        help='Resume the training from snapshot')
-    parser.add_argument('--unit', '-u', type=int, default=1000,
-                        help='Number of units')
-    args = parser.parse_args()
+_, test = chainer.datasets.get_mnist()
 
-    print('GPU: {}'.format(args.gpu))
-    print('# unit: {}'.format(args.unit))
-    print('# Minibatch-size: {}'.format(args.batchsize))
-    print('# epoch: {}'.format(args.epoch))
-    print('')
-
-    # Set up a neural network to train
-    # Classifier reports softmax cross entropy loss and accuracy at every
-    # iteration, which will be used by the PrintReport extension below.
-    model = MLP(args.unit, 10)
-    if args.gpu >= 0:
-        # Make a specified GPU current
-        chainer.cuda.get_device_from_id(args.gpu).use()
-        model.to_gpu()  # Copy the model to the GPU
-
-    # Setup an optimizer
-    optimizer = chainer.optimizers.Adam()
-    optimizer.setup(model)
-
-    # Load the MNIST dataset
-    train, test = chainer.datasets.get_mnist()
-
-    test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
-                                                 repeat=False, shuffle=False)
-
-    for img, label in tqdm.tqdm(test):
-        x_data = chainer.Variable(img[np.newaxis,:])
-        ret = F.softmax(model(x_data)).data
-
-if __name__ == '__main__':
-    main()
+for img, label in tqdm.tqdm(test):
+    x_data = chainer.Variable(img[np.newaxis,:])
+    ret = F.softmax(model(x_data)).data
