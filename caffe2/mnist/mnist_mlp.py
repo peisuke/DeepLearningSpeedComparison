fa21272f778869b784825e80f4bf3606f916976e
peisuke <peisuke.com@gmail.com>
2017-09-27 06:10:11 +0000

    fix

--- caffe2/mnist/mnist_mlp.py
+++ caffe2/mnist/mnist_mlp.py
@@ -5,64 +5,7 @@ import caffe2.python.predictor.predictor_exporter as pe
 import tqdm
 
 from caffe2.python import core, model_helper, net_drawer, workspace, visualize, brew
-
-# If you would like to see some really detailed initializations,
-# you can change --caffe2_log_level=0 to --caffe2_log_level=-1
-core.GlobalInit(['caffe2', '--caffe2_log_level=0'])
-
-# This section preps your image and test set in a lmdb database
-def DownloadResource(url, path):
-    '''Downloads resources from s3 by url and unzips them to the provided path'''
-    #import requests, zipfile, StringIO
-    import requests, zipfile
-    from io import BytesIO
-    print("Downloading... {} to {}".format(url, path))
-    r = requests.get(url, stream=True)
-    z = zipfile.ZipFile(BytesIO(r.content))
-    z.extractall(path)
-    print("Completed download and extraction.")
-    
-    
-current_folder = './'
-data_folder = os.path.join(current_folder, 'tutorial_data', 'mnist')
-root_folder = os.path.join(current_folder, 'tutorial_files', 'tutorial_mnist')
-db_missing = False
-
-if not os.path.exists(data_folder):
-    os.makedirs(data_folder)   
-    print("Your data folder was not found!! This was generated: {}".format(data_folder))
-
-# Look for existing database: lmdb
-if os.path.exists(os.path.join(data_folder,"mnist-train-nchw-lmdb")):
-    print("lmdb train db found!")
-else:
-    db_missing = True
-    
-if os.path.exists(os.path.join(data_folder,"mnist-test-nchw-lmdb")):
-    print("lmdb test db found!")
-else:
-    db_missing = True
-
-# attempt the download of the db if either was missing
-if db_missing:
-    print("one or both of the MNIST lmbd dbs not found!!")
-    db_url = "http://download.caffe2.ai/databases/mnist-lmdb.zip"
-    try:
-        DownloadResource(db_url, data_folder)
-    except Exception as ex:
-        print("Failed to download dataset. Please download it manually from {}".format(db_url))
-        print("Unzip it and place the two database folders here: {}".format(data_folder))
-        raise ex
-
-if os.path.exists(root_folder):
-    print("Looks like you ran this before, so we need to cleanup those old files...")
-    shutil.rmtree(root_folder)
-    
-os.makedirs(root_folder)
-workspace.ResetWorkspace(root_folder)
-
-print("training data folder:" + data_folder)
-print("workspace root folder:" + root_folder)
+from mnist_data import DownloadMNIST
 
 def AddInput(model, batch_size, db, db_type):
     data_uint8, label = model.TensorProtosDBInput(
@@ -81,6 +24,10 @@ def AddLeNetModel(model, data):
     pred = brew.fc(model, fc2, 'fc3', dim_in=1000, dim_out=10)
     softmax = brew.softmax(model, pred, 'softmax')
     return softmax
+
+core.GlobalInit(['caffe2', '--caffe2_log_level=0'])
+root_folder, data_folder = DownloadMNIST()
+workspace.ResetWorkspace(root_folder)
 
 arg_scope = {"order": "NCHW"}
 test_model = model_helper.ModelHelper(
