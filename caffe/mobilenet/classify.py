46a499eef5d7af259c3336974e39c8f8feedf58b
peisuke <peisuke.com@gmail.om>
2017-09-27 07:47:25 +0000

    caffe time

--- caffe/mobilenet/classify.py
+++ caffe/mobilenet/classify.py
@@ -3,6 +3,8 @@ import PIL.Image
 from StringIO import StringIO
 import numpy as np
 import sys
+import tqdm
+import time
 
 caffe_root = os.getenv('CAFFE_ROOT')
 sys.path.insert(0, caffe_root + 'python')
@@ -13,30 +15,13 @@ PRETRAINED = './mobilenet.caffemodel'
 
 caffe.set_mode_cpu()
 
-mean = np.array([103.94,116.78,123.68], dtype=np.float32)
-channel_swap = (2,1,0)
-raw_scale = 255
-image_dims=(224, 224)
-
 cnn = caffe.Net(MODEL_FILE, PRETRAINED, caffe.TEST)
 
-in_ = cnn.inputs[0]
-transformer = caffe.io.Transformer({in_: cnn.blobs[in_].data.shape})
-transformer.set_transpose(in_, (2, 0, 1))
-transformer.set_raw_scale(in_, raw_scale)
-transformer.set_mean(in_, mean)
-transformer.set_channel_swap(in_, channel_swap)
-
-input_image = caffe.io.load_image('../vgg16/cat.jpg')
-input_image = caffe.io.resize_image(input_image, image_dims)
-input_image = transformer.preprocess(cnn.inputs[0], input_image)
-input_image = input_image[np.newaxis, :, :, :]
-
-cnn.blobs['data'].data[...] = input_image 
-output = cnn.forward()
-pred = output[cnn.outputs[0]][0]
-
-# load ImageNet labels
-labels = np.loadtxt('synset_words.txt', str, delimiter='\t')
-
-print(labels[pred.argmax()], pred.max())
+nb_itr = 20
+timings = []
+for i in tqdm.tqdm(range(nb_itr)):
+    cnn.blobs['data'].data[...] = np.random.randn(1, 3, 224, 224).astype(np.float32) 
+    start_time = time.time()
+    output = cnn.forward()
+    timings.append(time.time() - start_time)
+print('%10s : %f (sd %f)'% ('caffe-vgg-16', np.array(timings).mean(), np.array(timings).std()))
